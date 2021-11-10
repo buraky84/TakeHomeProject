c01cfbfdcd3e7bd337cc73f8824a8c2b12f9871e
buraky84 <burakyilmaza@gmail.com>
2021-11-10 20:27:54 +0300

    integrate non used page templates

--- src/ui/pages/invest/Index.js
+++ src/ui/pages/invest/Index.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {HeaderContainer} from '../../components/HeaderContainer';
 
 export const Invest = () => {
-  return <></>;
+  return (
+    <View style={{flex: 1}}>
+      <HeaderContainer title={'Invest'} />
+      <View style={styles.mainContainer} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {flex: 1, backgroundColor: '#14121E', paddingHorizontal: 24},
+});
--- src/ui/pages/news/Index.js
+++ src/ui/pages/news/Index.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {HeaderContainer} from '../../components/HeaderContainer';
 
 export const News = () => {
-  return <></>;
+  return (
+    <View style={{flex: 1}}>
+      <HeaderContainer title={'News'} />
+      <View style={styles.mainContainer} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {flex: 1, backgroundColor: '#14121E', paddingHorizontal: 24},
+});
